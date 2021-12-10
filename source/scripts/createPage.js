61c5001fea972e8e7ea728e25d3fe071b29dc033
celiaxiao <c6xiao@ucsd.edu>
2021-12-10 12:06:18 -0800

    syncing with main

--- source/scripts/createPage.js
+++ source/scripts/createPage.js
@@ -1,124 +1,168 @@
-// This script will take the user's input with their recipe data in editCreate.html, and will send it to the server to be saved.
-import { insertRecipe } from "./APICalls.js"
-import { redirectRecipeDetail, routerNavigateWrapper, userData } from "./index.js";
-import { RECIPE_ROUTE, HOME_ROUTER } from './util.js'
-export default { setupCreatePage }
+/**
+ * Filename: createPage.js
+ * Adds event listeners to the add and delete ingredint and save buttons,
+ * in order have ingredient and step form elements behave correctly.
+ * Adds event listeners and routers to cancel and save buttons.
+ * Handles recipe object creation and saving to database on form submit with save button.
+ * 
+ * @file Tie api to save button, make form buttons funcitonal.
+ * @since 12.09.21
+ */
+
+import { insertRecipe } from "./APICalls.js";
+import {
+    redirectRecipeDetail,
+    routerNavigateWrapper,
+    userData,
+} from "./index.js";
+import {
+    RECIPE_ROUTE,
+    HOME_ROUTER,
+} from "./util.js";
+export default { setupCreatePage };
+
+/**
+ * Adds event listeners to ingrent, step, cancel and save buttons,
+ * in order to tie corresponding functions.
+ */
 export function setupCreatePage() {
-    // console.log("setupCreatePage() called");
 
-    // Submitting the entire recipe
-    const cancelBtn = document.getElementById("cancel")
-    cancelBtn.addEventListener('click', () => {
+    // Cancel button routes to home page
+    const cancelBtn = document.getElementById("cancel");
+    cancelBtn.addEventListener("click", () => {
+        routerNavigateWrapper(HOME_ROUTER);
+    });
 
-        routerNavigateWrapper(HOME_ROUTER)
-    })
+    
     const recipeForm = document.getElementById("recipeForm");
     recipeForm.onsubmit = onSubmitRecipe;
     clearRecipePage();
-    if (document.querySelector("#recipeForm #addIngr").getAttribute('listener') !== 'true') {
-        document.querySelector("#recipeForm #addIngr").setAttribute('listener', 'true');
-        document.querySelector("#recipeForm #addIngr").addEventListener("click", function() {
-            appendIngredient();
-        });
-        document.querySelector("#recipeForm #addStep").addEventListener("click", function() {
-            appendStep();
-        });
-        document.querySelector("#recipeForm #delIngr").addEventListener("click", function() {
-            deleteIngredient();
-        });
-        document.querySelector("#recipeForm #delStep").addEventListener("click", function() {
-            deleteStep();
-        });
+    if (
+        document
+            .querySelector("#recipeForm #addIngr")
+            .getAttribute("listener") !== "true"
+    ) {
+        document
+            .querySelector("#recipeForm #addIngr")
+            .setAttribute("listener", "true");
+        document
+            .querySelector("#recipeForm #addIngr")
+            .addEventListener("click", function () {
+                appendIngredient();
+            });
+        document
+            .querySelector("#recipeForm #addStep")
+            .addEventListener("click", function () {
+                appendStep();
+            });
+        document
+            .querySelector("#recipeForm #delIngr")
+            .addEventListener("click", function () {
+                deleteIngredient();
+            });
+        document
+            .querySelector("#recipeForm #delStep")
+            .addEventListener("click", function () {
+                deleteStep();
+            });
     }
-
 }
 
 let numSteps = 0;
 let numIngredients = 0;
-const onSubmitRecipe = async(event) => {
 
-    // console.log("SUBMITTED NEW RECIPE");
+/**
+ * Gathers the input fromt the form into a formData object,
+ * and saves it to database.
+ * 
+ * @param event
+ */
+const onSubmitRecipe = async (event) => {
+
     event.preventDefault();
     const recipeF = document.getElementById("recipeForm");
     let formData = new FormData(recipeF);
 
-    // get ingredients from form
     let stepsArr = [];
-    //should be empty array if no input
-    let strTags = formData.get('tags') ? formData.get('tags').replace(/\s+/g, '').split(/[;,.]+/) : [];
-    //let tagsArr = strTags.split(',');
 
+    //Pull tags from form
+    let strTags = formData.get("tags")
+        ? formData
+              .get("tags")
+              .replace(/\s+/g, "")
+              .split(/[;,.]+/)
+        : [];
+
+    // Pull ingredients from form
     let ingWithAmountArr = {};
     for (let i = 0; i < numIngredients; i++) {
-        ingWithAmountArr[formData.get('ingredient' + i)] = formData.get('ingredientAmount' + i);
-        /** 
-        ingrArr.push(formData.get('ingredient'+i));
-        ingrAmountArr.push(formData.get('ingredientAmount'+i));
-        console.log(formData.get('ingredient'+i));
-        console.log(formData.get('ingredientAmount'+i));
-        */
+        ingWithAmountArr[formData.get("ingredient" + i)] = formData.get(
+            "ingredientAmount" + i
+        );
     }
-    // console.log(ingArr);
 
-    // get steps from form
+    // Pull steps from form
     for (let i = 0; i < numSteps; i++) {
-        stepsArr.push(formData.get('step' + i));
-        console.log(formData.get('step' + i));
+        stepsArr.push(formData.get("step" + i ));
     }
 
-    // DANICA's attempt in uploading image
-    const recipeCard = document.createElement('recipe-card');
-    //console.log(document.getElementsByName('picture')[0].files[0]);
+    // Upload image
+    const recipeCard = document.createElement("recipe-card");
+
     let pic = null;
-    let img = null
-    if (document.getElementsByName('picture')[0].files.length > 0) {
-        pic = document.getElementsByName('picture')[0].files[0];
-        // img = window.URL.createObjectURL(fileObj);
+    let img = null;
+    if (document.getElementsByName("picture")[0].files.length > 0) {
+        pic = document.getElementsByName("picture")[0].files[0];
     }
 
-    // CREATE NEW RECIPE
+    // Create new recipe object
     let newRecipe = {
-        name: formData.get('name'),
+        name: formData.get("name"),
         datePosted: Date.now(),
-        image: formData.get('picture'),
+        image: formData.get("picture"),
         author: userData._id,
-        description: formData.get('description'),
+        description: formData.get("description"),
         tags: strTags,
-        servingSize: formData.get('servingSize'),
-        cookTime: formData.get('cookTime'),
+        servingSize: formData.get("servingSize"),
+        cookTime: formData.get("cookTime"),
         ingredients: ingWithAmountArr,
-        difficulty: formData.get('difficulty'),
-        steps: stepsArr
-    }
-    console.log(newRecipe);
-    // get response from POST API, get the new recipe, 
+        difficulty: formData.get("difficulty"),
+        steps: stepsArr,
+    };
+    
+    // Get response from POST API, get the new recipe,
     const responseRecipe = await insertRecipe(newRecipe);
     responseRecipe.author = userData;
-    redirectRecipeDetail(responseRecipe)
+    redirectRecipeDetail(responseRecipe);
     userData.myRecipe.push(responseRecipe);
     const page = responseRecipe._id;
-    const routeUrl = RECIPE_ROUTE + page
-    routerNavigateWrapper(routeUrl)
+    const routeUrl = RECIPE_ROUTE + page;
+    routerNavigateWrapper(routeUrl);
 };
 
+/**
+ * Adds a new step form element.
+ * Increments numSteps.
+ */
 const appendStep = () => {
-    //let d = document.getElementById('steps');
-    // d.innerHTML += "<input type='text' id='tst"+ x++ +"'><br >";
-    console.log("APPEND STEP");
     var newTextBox = document.createElement("div");
 
     newTextBox.innerHTML =
-        "<textarea cols='40' rows='4' id='textAreaBox' name='step" + numSteps + "' placeholder='Step #" +
+        "<textarea cols='40' rows='4' id='textAreaBox' name='step" +
+        numSteps +
+        "' placeholder='Step #" +
         numSteps +
         "'></textarea>";
     document.getElementById("newStepId").appendChild(newTextBox);
+    
     numSteps++;
-
 };
 
+/**
+ * Called when user deletes a step and reflects that on the page.
+ * Removes step element and decreases numSteps.
+ */
 const deleteStep = () => {
-    //newTextBox.classList.add('stepEntry');
-    console.log("DELETED STEP");
     if (document.getElementById("newStepId").lastChild != null) {
         document
             .getElementById("newStepId")
@@ -127,24 +171,36 @@ const deleteStep = () => {
     }
 };
 
+/**
+ * Adds a new ingredient and ingredient amounts form elements.
+ * Increments numIngredients.
+ */
 const appendIngredient = () => {
-    console.log("APPEND INGREDIENT");
+
     var newTextBox = document.createElement("div");
     newTextBox.innerHTML =
-        "<input type='text' id='newInputBox' name='ingredient" + numIngredients + "' placeholder='ingredient'>";
+        "<input type='text' id='newInputBox' name='ingredient" +
+        numIngredients +
+        "' placeholder='ingredient'>";
     document.getElementById("newIngredientId").appendChild(newTextBox);
 
     var newAmountBox = document.createElement("div");
     newAmountBox.innerHTML =
-        "<input type='text' id='newInputBox' name='ingredientAmount" + numIngredients + "' placeholder='amount (ie. 5 cups)'>";
+        "<input type='text' id='newInputBox' name='ingredientAmount" +
+        numIngredients +
+        "' placeholder='amount (ie. 5 cups)'>";
     document.getElementById("newIngredientAmountId").appendChild(newAmountBox);
     numIngredients++;
 };
 
+/**
+ * Called when user deletes an ingredient and reflects that on the page.
+ * Removes ingredient and amount elements and decreases numIngredients.
+ */
 const deleteIngredient = () => {
-    console.log("DELETE INGREDIENT");
     if (document.getElementById("newIngredientId").lastChild != null) {
         numIngredients--;
+
         document
             .getElementById("newIngredientId")
             .removeChild(document.getElementById("newIngredientId").lastChild);
@@ -155,12 +211,13 @@ const deleteIngredient = () => {
             );
     }
 };
+
+/**
+ * Remove form entries, steps and ingredients from previous page visit.
+ */
 const clearRecipePage = () => {
-
-    console.log("CLEARED");
-
-    document.getElementById('recipeForm').reset();
-
+    document.getElementById("recipeForm").reset();
+    
     for (let i = 0; i <= numSteps; i++) {
         deleteStep();
     }
--- source/scripts/indexSearch.js
+++ source/scripts/indexSearch.js
@@ -1,34 +1,42 @@
-//This file is when the user searches a keyword in index.html page
-import { createNodeClone } from './util.js';
-export default { init, initialRecipeCards }
-if (typeof window === 'object') {
+/**
+ * Filename: indexSearch.js
+ * 
+ * @file Populates tags and binds search functionality to the search bar and tags.
+ * @since 12.09.21
+ */
+import { createNodeClone } from "./util.js";
+export default { init, initialRecipeCards };
+if (typeof window === "object") {
     window.addEventListener("DOMContentLoaded", init);
 }
-import { submitSearch, submitInitialSearch } from "./APICalls.js"
+import {
+    submitSearch,
+    submitInitialSearch,
+ } from "./APICalls.js";
+
 import { createRecipeCards } from "./index.js";
 
-let allTags = ["Easy", "Intermediate", "Hard", "Vegetarian", "Breakfast", "Lunch", "Dinner", "Appetizer", "Dessert", "Vegan"];
-let difficultyTags = ["Easy", "Intermediate", "Hard"];
-let mealTimeTags = ["Breakfast", "Lunch", "Dinner", "Appetizer", "Dessert"];
-let dietaryTags = ["Vegetarian", "Vegan"];
+const difficultyTags = ["Easy", "Intermediate", "Hard"];
+const mealTimeTags = ["Breakfast", "Lunch", "Dinner", "Appetizer", "Dessert"];
+const dietaryTags = ["Vegetarian", "Vegan"];
 let selectedTags = [];
 let curr_page = 1;
 let max_page = 1;
+
 /**
- * initializes search functionality through the frontend
+ * Ties search functionality to the search bar, buttons and tags,
+ * populates tags and adds click event listener for styling and search.
  */
 async function init() {
-    // TODO: Make sure this works for both landing and home pages
-    createNodeClone('#searchBar', true);
+    
+    //Search bar and button
+    createNodeClone("#searchBar", true);
+    createNodeClone("#searchButton", true);
     let searchBar = document.querySelector("#searchBar");
-    createNodeClone('#searchButton', true);
     let searchButton = document.querySelector("#searchButton");
-    let scrollLeftButton = document.querySelector("#scrollLeft");
-    let scrollRightButton = document.querySelector("#scrollRight");
 
     searchButton.addEventListener("click", async function () {
         submitNewSearch();
-
     });
 
     searchBar.addEventListener("keydown", async function (event) {
@@ -39,91 +47,112 @@ async function init() {
         }
     });
 
+    //Page scroll buttons
+    let scrollLeftButton = document.querySelector("#scrollLeft");
+    let scrollRightButton = document.querySelector("#scrollRight");
+
     scrollRightButton.addEventListener("click", async function () {
         if (curr_page < max_page) {
             curr_page++;
-            let results = await submitSearch(searchBar.value, selectedTags, curr_page);
+            let results = await submitSearch(
+                searchBar.value,
+                selectedTags,
+                curr_page
+            );
             createRecipeCards(results);
-            document.getElementById('homePageNum').innerHTML = 'Page: ' + curr_page + ' / ' + max_page;
+            document.getElementById("homePageNum").innerHTML =
+                "Page: " + curr_page + " / " + max_page;
         }
     });
 
     scrollLeftButton.addEventListener("click", async function () {
         if (curr_page > 1) {
             curr_page--;
-            let results = await submitSearch(searchBar.value, selectedTags, curr_page);
+            let results = await submitSearch(
+                searchBar.value,
+                selectedTags,
+                curr_page
+            );
             createRecipeCards(results);
-            document.getElementById('homePageNum').innerHTML = 'Page: ' + curr_page + ' / ' + max_page;
+            document.getElementById("homePageNum").innerHTML =
+                "Page: " + curr_page + " / " + max_page;
         }
     });
 
+    //Populate tags
     let tagsSelect = document.getElementById("tagsTitles");
-    // console.log(tagsSelect);
     if (tagsSelect) {
-        addTags(this);
+        addTags();
         let tagButtons = document.getElementsByClassName("tagButton");
         for (let i = 0; i < tagButtons.length; i++) {
-            //e.stopPropagation();
-            tagButtons[i].addEventListener('click', (e) => {
+            tagButtons[i].addEventListener("click", () => {
                 clickedOnATag(tagButtons[i]);
             });
-            /** 
-                        tagButtons[i].addEventListener('mouseover', (e) => {
-                            onHover(tagButtons[i]);
-                        });
-                        tagButtons[i].addEventListener('mouseout', (e) => {
-                            offHover(tagButtons[i]);
-                        });
-                        */
-
         }
     }
 }
 
+/**
+ * Refreshes displayed recipes with new search results.
+ */
 async function submitNewSearch() {
-    let searchResults = await submitInitialSearch(searchBar.value, selectedTags);
+    /* eslint-disable no-undef */
+    let searchResults = await submitInitialSearch(
+        searchBar.value,
+        selectedTags
+    );
+    /* eslint-enable no-undef */
     createRecipeCards(searchResults.results);
     curr_page = 1;
     max_page = searchResults.pages.pages;
-    document.getElementById('homePageNum').innerHTML = 'Page: ' + curr_page + ' / ' + max_page;
+    document.getElementById("homePageNum").innerHTML =
+        "Page: " + curr_page + " / " + max_page;
 }
 
-function addTags(e) {
-
+/**
+ * Populates the tags into a grid by category.
+ */
+function addTags() {
+    
     //Difficulty
     let tagsList = document.getElementById("tagsListDiff");
     tagsList.style.display = "grid";
-    for (let i of difficultyTags) {
+    for (let name of difficultyTags) {
         let addTagButton = document.createElement("button");
         addTagButton.classList.add("tagButton");
-        addTagButton.innerText = i;
-        addTagButton.id = i;
+        addTagButton.innerText = name;
+        addTagButton.id = name;
         tagsList.appendChild(addTagButton);
     }
 
     //Meal Time
     tagsList = document.getElementById("tagsListMt");
     tagsList.style.display = "grid";
-    for (let i of mealTimeTags) {
+    for (let name of mealTimeTags) {
         let addTagButton = document.createElement("button");
         addTagButton.classList.add("tagButton");
-        addTagButton.innerText = i;
-        addTagButton.id = i;
+        addTagButton.innerText = name;
+        addTagButton.id = name;
         tagsList.appendChild(addTagButton);
     }
 
     //Dietary
     tagsList = document.getElementById("tagsListDiet");
     tagsList.style.display = "grid";
-    for (let i of dietaryTags) {
+    for (let name of dietaryTags) {
         let addTagButton = document.createElement("button");
         addTagButton.classList.add("tagButton");
-        addTagButton.innerText = i;
-        addTagButton.id = i;
+        addTagButton.innerText = name;
+        addTagButton.id = name;
         tagsList.appendChild(addTagButton);
     }
 }
 
+/**
+ * Populates the tags into a grid by category.
+ * 
+ * @param e The tag element
+ */
 function clickedOnATag(e) {
     let index = selectedTags.indexOf(e.innerText);
     if (index > -1) {
@@ -139,29 +168,9 @@ function clickedOnATag(e) {
     }
     submitNewSearch();
 }
-/** 
-function onHover(e) {
-    let index = selectedTags.indexOf(e.innerText);
-    if (index > -1) {
-        e.style.backgroundColor = "#f9c784";
-        e.style.borderColor = "#f9c784";
-    } else {
-        e.style.backgroundColor = "#4e598c";
-        e.style.borderColor = "#4e598c";
-    }
-}
-
-function offHover(e) {
-    let index = selectedTags.indexOf(e.innerText);
-    if (index > -1) {
-        e.style.backgroundColor = "#fcaf58";
-        e.style.borderColor = "#fcaf58";
-    } 
-}
-*/
 
 /**
- * Initial render of recipe cards. 
+ * Initial render of recipe cards.
  */
 export async function initialRecipeCards() {
     submitNewSearch();
